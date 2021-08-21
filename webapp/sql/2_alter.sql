261e0f3f4d1451d585fdc3e25bbe947331964387
akouryy <akouryy7@gmail.com>
2021-08-21 14:18:04 +0900

    Fix SQL type

--- webapp/sql/2_alter.sql
+++ webapp/sql/2_alter.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `isu_condition` ADD COLUMN `cond_dirty` BOOLEAN GENERATED ALWAYS AS (FIND_IN_SET("is_dirty=true",`condition`)) STORED;
-ALTER TABLE `isu_condition` ADD COLUMN `cond_overweight` BOOLEAN GENERATED ALWAYS AS (FIND_IN_SET("is_overweight=true",`condition`)) STORED;
-ALTER TABLE `isu_condition` ADD COLUMN `cond_broken` BOOLEAN GENERATED ALWAYS AS (FIND_IN_SET("is_broken=true",`condition`)) STORED;
+ALTER TABLE `isu_condition` ADD COLUMN `cond_dirty` BOOLEAN GENERATED ALWAYS AS (FIND_IN_SET("is_dirty=true",`condition`) > 0) STORED;
+ALTER TABLE `isu_condition` ADD COLUMN `cond_overweight` BOOLEAN GENERATED ALWAYS AS (FIND_IN_SET("is_overweight=true",`condition`) > 0) STORED;
+ALTER TABLE `isu_condition` ADD COLUMN `cond_broken` BOOLEAN GENERATED ALWAYS AS (FIND_IN_SET("is_broken=true",`condition`) > 0) STORED;
 ALTER TABLE `isu_condition` ADD COLUMN `cond_level` TINYINT GENERATED ALWAYS AS (cond_dirty + cond_overweight + cond_broken) STORED;
