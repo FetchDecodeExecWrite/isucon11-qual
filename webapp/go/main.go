ea6c9ba8f793887bcfbab68cc09a01cec1a4f710
moratorium08 <moratorium08@gmail.com>
2021-08-21 15:50:56 +0900

    log-off

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -214,8 +214,8 @@ func init() {
 
 func main() {
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
+	e.Debug = false
+	e.Logger.SetLevel(log.OFF)
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
