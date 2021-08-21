87b72192e2d4d5572336690d63ba1f68b51eac90
moratorium08 <moratorium08@gmail.com>
2021-08-21 11:24:34 +0900

    Update waiwai

--- waiwai.sh
+++ waiwai.sh
@@ -13,7 +13,9 @@ for i in 1 2 3; do ssh isu$i -- "git fetch origin $branch"; done
 for i in 1 2 3; do ssh isu$i -- "git checkout $branch 2>/dev/null || git checkout -b $branch"; done
 for i in 1 2 3; do ssh isu$i -- "git reset --hard origin/$branch"; done
 
-for i in 1 2 3; do ssh isu$i -- "source bashrc2 && cd ~/webapp/go; go build"; done
 echo "----------pull $branch complete.----------"
+
+for i in 1 2 3; do echo "- isu$i build..."; ssh isu$i -- "source ~/bashrc2 && cd ~/webapp/go; go build"; done
+
 for i in 1 2 3; do ssh isu$i -- "sudo systemctl stop isucondition.go.service"; done
 for i in 1 2 3; do ssh isu$i -- "sudo systemctl start isucondition.go.service"; done
