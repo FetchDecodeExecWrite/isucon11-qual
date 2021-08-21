fd89d0fc281ae5076286a4507d3e9890e5908c63
akouryy <akouryy7@gmail.com>
2021-08-21 11:33:08 +0900

    Revert "アイコンのアクセス制限を解除"
    This reverts commit 4736443922dfd077152b7f32e97ac6f90aa4ff31.

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -688,22 +688,21 @@ func getIsuID(c echo.Context) error {
 // GET /api/isu/:jia_isu_uuid/icon
 // ISUのアイコンを取得
 func getIsuIcon(c echo.Context) error {
-	var err error
-	// jiaUserID, errStatusCode, err := getUserIDFromSession(c)
-	// if err != nil {
-	// 	if errStatusCode == http.StatusUnauthorized {
-	// 		return c.String(http.StatusUnauthorized, "you are not signed in")
-	// 	}
+	jiaUserID, errStatusCode, err := getUserIDFromSession(c)
+	if err != nil {
+		if errStatusCode == http.StatusUnauthorized {
+			return c.String(http.StatusUnauthorized, "you are not signed in")
+		}
 
-	// 	c.Logger().Error(err)
-	// 	return c.NoContent(http.StatusInternalServerError)
-	// }
+		c.Logger().Error(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
 
 	jiaIsuUUID := c.Param("jia_isu_uuid")
 
 	var image []byte
-	err = db.Get(&image, "SELECT `image` FROM `isu` WHERE `jia_isu_uuid` = ?",
-		jiaIsuUUID)
+	err = db.Get(&image, "SELECT `image` FROM `isu` WHERE `jia_user_id` = ? AND `jia_isu_uuid` = ?",
+		jiaUserID, jiaIsuUUID)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return c.String(http.StatusNotFound, "not found: isu")
